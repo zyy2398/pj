e14280dddd7ee2025f38d45f5dabe6e7f7381084
张雍逸 <zyy2398@outlook.com>
2020-09-06 11:25:10 +0800

    上传文件

---
+++ pj/OneManager/config.php
@@ -0,0 +1,26 @@
+<?php $configs = '
+{
+    "admin": "zyy2398",
+    "background": "aHR0cCUzQSUyRiUyRjE3MTV6eXkueGxwaHAubmV0JTJGT25lTWFuYWdlciUyRnp5eTIzOTglMkZ1bnRpdGxlZC5wbmc",
+    "disktag": "outlook|zyy2398",
+    "hideFunctionalityFile": "1",
+    "outlook": {
+        "Drive_ver": "MS",
+        "diskname": "YmRpYW1vbmQyJTQwc3R1ZGVudC5jY2MuZWR1",
+        "guestup_path": "JTJGcGljdHVwbGQ",
+        "refresh_token": "OAQABAAAAAAAGV_bv21oQQ4ROqh0_1-tAn_-iQehM_Lw_lnerMEGEweNwy4yDp23dCxE1LH1ElQ95eUW8OnquD34ATGbWsU4_xpcDQhn9a_CP3rYbGJnvW-lVb215LdCDeKWoqPRtl3CBhbse94opfRCXbjzYHqhzjh-gGghGmY3dnFSffplYP5fuZsdJZBZ8P--DqWfo8bjPUgZ8qrq4I3yb7pkZe0ZCxfhmi7lSklSaVESpo9yDdPKNESVLzm5iHPl1bLvuj1gC1EYpRxUfbHzmpggRg-Nl0nWTeGUsfq1C-xZPoKXqY0s4g-RCkoYrTCp6twgutErlwWew3jZSUk_zNWGb6ZYTdiDwmQa8KRwoP49pfzPIlgYQA7gnHkzTC-KvtkOfu26uSGvP5Gh0g4TuIDqAekPN-yP23mFZ8Xx1kyVnO1LR03PFBNhNjyioRf95AJepNNfG569Tdja77QtCnzo8wC25LEsBwr3AVzrdKLjIBAma3L1YhqPHcpa2a3u98AQV0e9YkMp0mhoZKMkp3ejKMScZpr7O8MWFhb8cc1wJ-D7Co0MXLiG5p_H8iTNA4Wm-PVXRiB8G7WPOjKMJu2UuIV_g1omQhgjwCjXgKN-45WniT8EdZYQi6Iq9yZDvRiw-sUd5e_Lmvm6Jo4hm2AxQbqSrnYGkR3NnZT5Z-VmqSKyemuib9ixRiFITxCyAQszSRMEcKF5GwwuB39JFMwFN8n0tI5MQu5Txx9_A6bycpk8eFon2iJM8hPObz51xtMWTJqTkA-HnZD2jaL4sde0Vx9GTjJbNvOZK1mjGOfb9NSO0uDBU1Nf8Vbsozm9NutG_4m6KMm_4gVk4wwx_K5WL0PIS2LfYcyjkTUL053e6ppLK5Kx4xu5qdYsfQgbjSdK8EFBESBFRAqxVwmNcHA3KVQtiAfOs1U-g-EpG09eSUPNMTR1xUMHI_38XI8xsptaPszqp8RfXvopM36GtOIELXI_LW3OVsORTFT6Bg4kIspOGj04euJGnTd_AfMbiuMOGkHQgAA",
+        "token_expires": 1599043412
+    },
+    "passfile": "zyy2398@t.odmail.cn",
+    "sitename": "T25lRHJpdmUrb2YrMTcxNSVFNSVCQyVBMCVFOSU5QiU4RCVFOCVCRiU5Qw",
+    "theme": "onemoe.html",
+    "timezone": "8",
+    "zyy2398": {
+        "Drive_ver": "MS",
+        "diskname": "enl5MjM5OCU0MG91dGxvb2suY29t",
+        "guestup_path": "JTJGcGljdHVwbGQ",
+        "refresh_token": "M.R3_BAY.-CdhnXt*KeNO40TwLUUaGfdbdTpnaUE*pH9trDmJKSOFnCSPnV8LTNtyTQ81foCVcEIqpy4Fvv0BXdQ4*cZ!eJAixXtpPWlQk8NjfLluRD6fCv91rAeCS2reLHWAdyd*9XncxzQ6PxzlxY89*R5W2zgWLAQuh00gx4gtTr!ctO8I3QseVaTjojW4pkyOkVAIkrPY4K*q8x3MncfwkfgBeAuNIYEhMFd8pbXteJhXP5ro4ghkzWYRXC5ImRfXIxtnW8D9NHBb7HhXCE2qpjGCXCCn5oqD7M3QnLS6PFjbJSpFuCzBegnCQ2tAski7TNX6SanHIFnboWkyzq!R9hDAtS01Ofr9werHAllPtJRNjq9*4",
+        "token_expires": 1599105469
+    }
+}
+';
